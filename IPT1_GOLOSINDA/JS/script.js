5765ec1f6c30c1bfce74a0a9220caad595cb5fa6
BSIT3E_GOLOSINDA <aaronmhargolosinda@gmail.com>
2024-11-26 16:42:08 +0800

    PORTFOLIO

--- IPT1_GOLOSINDA/JS/script.js
+++ IPT1_GOLOSINDA/JS/script.js
@@ -1,12 +1,3 @@
 function main() {
 	const owner = "Aaron Mhar G. Golosinda";
 }
-function menupopup() {
-	document.getElementbyID("buttonContainer").innerHTML.style.display = "flex";
-	document.getElementbyID("buttonContainer").innerHTML.style.justifyContent = "space-evenly";
-	document.getElementbyID("buttonContainer").innerHTML.style.flexDirection = "row";
-}
-function menuhide() {
-
-}
-document.display = none;
